d8e5367bf721d5cdf2a837d6ea9bb718c608a012
Chayanonc <chayanonc.ph@gmail.com>
2024-01-18 23:33:46 +0700

    🎉 init: initial ghotie pj

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,66 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import accountUtils from "./utils/accountUtils";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.PRIVATE_KEY || "",
+      avalancheFujiTestnet: "snowtrace",
+      polygonMumbai: process.env.APIKEY_POLYGON || "",
+      bscTestnet: process.env.APIKEY_BSC || "",
+      optimisticGoerli: process.env.APIKEY_OP || "",
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    bkc_test: {
+      url: "https://rpc-testnet.bitkubchain.io",
+      accounts: accountUtils.getAccounts(),
+    },
+    bsc_test: {
+      url: "https://bsc-testnet.publicnode.com",
+      accounts: accountUtils.getAccounts(),
+    },
+    sepolia: {
+      url: "https://ethereum-sepolia.publicnode.com",
+      accounts: accountUtils.getAccounts(),
+    },
+    mumbai: {
+      url: "https://polygon-mumbai-bor.publicnode.com",
+      accounts: accountUtils.getAccounts(),
+    },
+    fuji: {
+      url: "https://avalanche-fuji-c-chain.publicnode.com",
+      accounts: accountUtils.getAccounts(),
+    },
+    avalanceFuji: {
+      url: "https://avalanche-fuji-c-chain.publicnode.com",
+      accounts: accountUtils.getAccounts(),
+    },
+    optimismGoerli: {
+      url: "https://optimism-goerli.publicnode.com",
+      accounts: accountUtils.getAccounts(),
+    },
+
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      accounts: accountUtils.getAccounts(),
+    },
+  },
 };
 
 export default config;
